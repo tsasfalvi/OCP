e3dec37474e6c1efaa9d7e8e9b98c443de30af4a
Tamas_Sasfalvi <tamas_sasfalvi@epam.com>
2017-05-23 10:12:32 +0200

    initial code

---
+++ src/main/java/com/epam/Animal.java
@@ -0,0 +1,5 @@
+package com.epam;
+
+public enum Animal {
+	DOG, CAT, DUCK;
+}
---
+++ src/main/java/com/epam/Application.java
@@ -0,0 +1,11 @@
+package com.epam;
+
+
+public class Application {
+	
+	public static void main(String[] args) {
+		Farm farm = new Farm();
+		farm.sing();
+	}
+	
+}
